72f1313165d6f4810c44125a6a09e1535f4a84af
Daniel Perez <desarrollosupervisa@gmail.com>
2019-08-29 15:27:12 -0500

    come on

--- src/server.js
+++ src/server.js
@@ -2,15 +2,17 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-
+const http = require('http');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/demo-deploy'));
+app.use(express.static(__dirname + '/dist/deploy-proyect'));
 
 app.get('/*', function(req,res) {    
-    res.sendFile(path.join(__dirname+'/dist/demo-deploy/index.html'));
+    res.sendFile(path.join(__dirname));
 });
+
+const server = http.createServer(app);
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
