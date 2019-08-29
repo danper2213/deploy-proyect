1fabadb792ccc3fe552667c320a2958c62d95035
Daniel Perez <desarrollosupervisa@gmail.com>
2019-08-29 15:04:14 -0500

    test4

--- src/server.js
+++ src/server.js
@@ -6,10 +6,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/deploy-proyect'));
+app.use(express.static(__dirname + '/dist/demo-deploy'));
 
 app.get('/*', function(req,res) {    
-    res.sendFile(path.join(__dirname+'/dist/deploy-proyect/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/demo-deploy/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
